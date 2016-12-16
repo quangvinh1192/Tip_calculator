7acaf4e7a10f57e3a76c0846e8fa427ff626befa
Vinh Tran <quangvinh1192@gmail.com>
2016-12-15 20:04:37 -0800

    basic function

--- Tips Calculatior/ViewController.swift
+++ Tips Calculatior/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var Total_label: UILabel!
+    @IBOutlet var Bill_label: UITextField!
+    @IBOutlet var Tip_label: UILabel!
+    @IBOutlet var Tip_control: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +26,20 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func Tap_control(_ sender: Any) {
+        //tap anywhere distap the keyboard
+        view.endEditing(true)
+    }
+
+    
+    @IBAction func re_calculate_tip(_ sender: AnyObject) {
+        let tip_percent = [0.18, 0.2, 0.25]
+        let bill = Double (Bill_label.text!) ?? 0
+        let tip = bill * tip_percent[Tip_control.selectedSegmentIndex]
+        let total = bill + tip
+        Tip_label.text = String(format: "$ %.2f", tip)
+        Total_label.text = String(format: "$ %.2f", total)
+        
+    }
 }
 
